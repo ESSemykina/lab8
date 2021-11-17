480457ce392b0cc3a292a8b4e1fac643fbb192d5
ESSemykina <90087300+ESSemykina@users.noreply.github.com>
2021-11-18 00:19:18 +0200

    Add files via upload

---
+++ 8.2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <fstream>
+#define N  100
+#define M  100
+int main()
+{
+	std::ifstream in("input.txt");
+
+	int** matrix = new int*[N];
+	for (int i = 0; i < N; i++)
+		matrix[i] = new int[M];
+	
+	int	min;
+	int min2;
+	int sum = 0;
+	min = INT_MAX;
+	int n, m;
+	in >> n >> m;
+
+	// Ввод
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			in >> matrix[i][j];
+
+
+	// Обработка
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			sum = sum + matrix[i][j];
+		}
+		if (sum < min)
+		{
+			min = sum;
+			min2 = i;
+			sum = 0;
+		}
+		sum = 0;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (i == min2)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				matrix[i][j] = min;
+			}
+		}
+	}
+
+
+	// Вывод
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+
+			std::cout << matrix[i][j] << " ";
+		}
+
+		std::cout << std::endl;
+	}
+
+
+	for (int i = 0; i < N; i++)
+		delete[] matrix[i];
+
+	delete[] matrix;
+
+	
+
+
+}
---
+++ 8.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+int main()
+{
+	
+	int n;
+	std::cin >> n;
+	int* M = new int[n];
+	int* first = new int[n];
+	int* min = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> M[i];
+		int x = M[i];
+		while (x > 9)
+			x /= 10;
+		first[i] = x;
+
+		x = M[i];
+		min[i] = 9;
+		while (x > 0)
+		{
+			int d = x % 10;
+			if (min[i] > d)
+				min[i] = d;
+			x = x / 10;
+		}
+	}
+	for (int i = 0; i > n; i++)
+		for (int j = i + 1; j > n; j++)
+			if (first[i] <= first[j])
+			{
+				std::swap(first[i], first[j]);
+			}
+			else if (first[i] == first[j] && min[i] <= min[j])
+			{
+				std::swap(min[i], min[j]);
+			}
+			else if (first[i] == first[j] && min[i] == min[j])
+			{
+				std::swap(min[i], min[j]);
+			}
+	for (int i = 0; i < n; i++)
+	{
+		std::cout << M[i] << " " << first[i] << " " << min[i] << std::endl;
+	}
+	delete M;
+	delete first;
+	delete min;
+}
